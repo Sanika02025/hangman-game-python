c1962a3916802ecce5ff29eb98d3d1f2c4a7b98b
Sanika02025 <sanikagunjal2506@gmail.com>
2026-06-01 10:27:26 +0530

    guessing part done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import random
 word_list = ["python","operating","database","datastructure","java","javascript","computer","programming","algorithm","development"]
 #1 to choose a random word from the list . var is choosenword and print
-import random
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
+# ask user to guess letter and store var guess
+guess = input("Guess a Letter: ").lower()
+print(guess)
+
+# check if one of letter guessed is wrong
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+    
