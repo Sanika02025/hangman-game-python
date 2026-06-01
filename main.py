65e9db794437715b175db59b07f1c5ec8b689449
Sanika02025 <sanikagunjal2506@gmail.com>
2026-06-01 10:21:37 +0530

    task 1 done randomly choose

---
+++ main.py
@@ -0,0 +1,5 @@
+word_list = ["python","operating","database","datastructure","java","javascript","computer","programming","algorithm","development"]
+#1 to choose a random word from the list . var is choosenword and print
+import random
+chosen_word = random.choice(word_list)
+print(chosen_word)
